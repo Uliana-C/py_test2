6f0ba61dba6d25dee9e9ed4998ff62ef91d94a38
Uliana-C <tyk240405@gmail.com>
2024-12-19 00:41:17 +0300

    описание боя

--- main2.py
+++ main2.py
@@ -1,1 +1,20 @@
-print("helllo")
+import random
+import time
+
+class Fighter:
+    def __init__(self, name: str, health: float, damage: list[float, float], attack_speed: list[float, float]):
+        self.name = name
+        self.health = health
+        self.damage = damage
+        self.attack_speed = attack_speed
+
+#Метод получения урона от бойца
+    def __sub__(self, other):
+        damage_taken = random.uniform(other.damage[0], other.damage[1])
+        self.health -= damage_taken
+        return damage_taken
+
+#Возвращает время до следующей атаки
+    def attack(self):
+        return random.uniform(self.attack_speed[0], self.attack_speed[1])
+
